89e0a67183ab910b79b4d6b24ec430bef3ddd186
carloslavig <carloslavig@gmail.com>
2024-11-03 21:53:29 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args)
+    {
+//        String nomeOriginal = " Bora codar";
+//        saudacao(nomeOriginal);
+        int resultado = soma(2,3);
+        System.out.println(resultado);
+    }
+
+    public static int soma(int a, int b)
+    {
+        return a+b;
+    }
+//    public static void saudacao(String nomeParametro)
+//    {
+//        System.out.println("Salve"+ nomeParametro);
+//    }
+}
